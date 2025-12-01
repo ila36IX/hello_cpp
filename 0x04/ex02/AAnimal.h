debb588cd01a0d7c39bae46fecd9e8aa5c664c29
ila36ix <jbariali002@gmail.com>
2025-12-01 11:23:37 +0100

    This one with dog and cat was an Ok

---
+++ 0x04/ex02/AAnimal.h
@@ -0,0 +1,22 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class AAnimal
+{
+      protected:
+        std::string type;
+
+      public:
+        AAnimal();
+        AAnimal(const AAnimal &other);
+        AAnimal &operator=(const AAnimal &other);
+        virtual ~AAnimal();
+
+        virtual void makeSound() const = 0;
+        std::string getType() const;
+};
+
+#endif
